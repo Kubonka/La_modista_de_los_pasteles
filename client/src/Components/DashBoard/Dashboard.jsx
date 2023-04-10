3b80400a42c2b3db740d15266b6942206765246b
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-04-10 19:36:48 -0300

    tabla

--- client/src/Components/DashBoard/Dashboard.jsx
+++ client/src/Components/DashBoard/Dashboard.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { createCake } from "../../redux/cakeSlice";
+import '../../styles/index.css';
+import Table from "./Table";
+
 function Dashboard() {
   const dispatch = useDispatch();
   const currentCake = useSelector((state) => state.cake.currentCake);
@@ -16,13 +19,36 @@ function Dashboard() {
     if (currentCake && currentCake.cake_id)
       navigate(`/managecake/${currentCake.cake_id}`);
   }, [currentCake]);
+
+  
   return (
-    <div>
-      Dashboard
-      <button onClick={handleCreateCake}> CREATE CAKE </button>
-      <button onClick={handleManageTags}> MANAGE TAGS </button>
+
+    <div className="flex flex-row h-screen bg-purple-200">
+    <div className="flex flex-col bg-pink-200 w-1/4 justify-center items-center ">
+
+      <div className="flex flex-col border-4 rounded-md border-black">
+      
+      <a href="#" className="py-4 px-6 bg-pink-200 text-gray-600 hover:bg-gray-400 hover:text-gray-700 ">
+        Administrar torta
+      </a>
+      <a href="#" className="py-4 px-6 bg-pink-200 text-gray-600 hover:bg-gray-400 hover:text-gray-700">
+        Administrar tags
+      </a>
+      <a href="#" className="py-4 px-6 bg-pink-200 text-gray-600 hover:bg-gray-400 hover:text-gray-700">
+        Administrar Carrusel
+      </a>
+      <a href="#" className="py-4 px-6 bg-pink-200 text-gray-600 hover:bg-gray-400 hover:text-gray-700">
+        Administrar Usuarios
+      </a>
+      </div>
+      
     </div>
-  );
+    
+    
+    <Table />
+  </div>
+  
+);
 }
 
 export default Dashboard;
